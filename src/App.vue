da73cab0882b3d30154e80a3faa464fb1bcd81f2
Gilson401 <c.disprosio@gmail.com>
2022-06-15 14:16:02 -0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div ref="actions" id="app">
+    <p>Selecione um emoji</p>
+    <div class="inputs">
+      <input type="text" v-model="textInput" />
+      <button type="button" @click="showPicker = !showPicker">
+        Show/Hide Emojis
+      </button>
+      <div class="emoji-selector">
+        <emoji-picker v-show="showPicker" @click="selectEmoji" ref="emoji" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "emoji-picker-element";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      showPicker: false,
+      textInput: "",
+    };
+  },
+  mounted() {
+    this.$refs.emoji.addEventListener("emoji-click", this.selectEmoji);
+    document.addEventListener("click", this.close);
+  },
+  beforeDestroy() {
+    this.$refs.emoji.removeEventListener("emoji-click", this.selectEmoji);
+    document.removeEventListener("click", this.close);
+  },
+  methods: {
+    selectEmoji(event) {
+      const { detail } = event;
+      if (typeof detail === "object") {
+        this.textInput = this.textInput.concat(detail.emoji.unicode);
+      }
+    },
+    close(e) {
+      this.$nextTick(() => {
+        if (this.$refs.actions && !this.$refs.actions.contains(e.target)) {
+          this.showPicker = false;
+        }
+      });
+    },
+  },
+};
 </script>
 
 <style>
@@ -24,5 +58,26 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.inputs {
+  position: relative;
+  display: inline-block;
+  width: auto;
+}
+.emoji-selector {
+  display: block;
+  position: absolute;
+  margin: 5px 0;
+}
+
+emoji-picker > .picker {
+  text-align: center;
+}
+
+@media screen and (max-width: 320px) {
+  emoji-picker {
+    --num-columns: 6;
+    --category-emoji-size: 1.125rem;
+  }
 }
 </style>
